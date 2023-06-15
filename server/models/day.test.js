ab0b8975c0985d2f2187c0277449eece23ae3fd1
Adam Parr <adamparr.uk@gmail.com>
2023-06-16 00:38:45 +0100

    add Day model tests, husky pre-commit hook

--- server/models/day.test.js
+++ server/models/day.test.js
@@ -1,11 +1,28 @@
 import { describe, it, expect } from '@jest/globals';
 import Day from './day.js';
 
-describe('day', () => {
+describe('Day model', () => {
   it('should be invalid if date is empty', () => {
     const d = new Day();
     const validationError = d.validateSync();
-    console.log(validationError);
     expect(validationError.errors.date).toBeDefined();
+  });
+
+  it('should be invalid if date is invalid', () => {
+    const d = new Day({
+      date: 'invalid_date',
+    });
+    const validationError = d.validateSync();
+    expect(validationError.errors.date).toBeDefined();
+  });
+
+  it('should be valid if date is provided', () => {
+    const d = new Day({
+      date: new Date(),
+      score: 1,
+      tags: ['test'],
+    });
+    const validationError = d.validateSync();
+    expect(validationError).toBeUndefined();
   });
 });
