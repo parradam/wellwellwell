1f857b01126add6fa1c330de33c9c0b2458fa441
Adam Parr <adamparr.uk@gmail.com>
2023-06-25 22:07:21 +0100

    write controller test for getDays

--- server/controllers/dayController.test.js
+++ server/controllers/dayController.test.js
@@ -1,72 +1,104 @@
 import { jest, describe, it, expect } from '@jest/globals';
-import { createDay } from './dayController.js';
+import { createDay, getDays } from './dayController.js';
 import Day from '../models/day.js';
 
 jest.mock('../models/day.js');
 
-describe('addDay', () => {
-  it('should add day information and return 201', async () => {
-    const req = {
-      body: {
-        date: '2023-06-04T20:49:58.178Z',
-        score: 1,
-        tags: ['bored', 'tired'],
-      },
-    };
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
+describe('Day controller', () => {
+  describe('create day', () => {
+    it('should add day information and return 201', async () => {
+      const req = {
+        body: {
+          date: '2023-06-04T20:49:58.178Z',
+          score: 1,
+          tags: ['bored', 'tired'],
+        },
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
 
-    const mockedDay = {
-      date: req.body.date,
-      score: req.body.score,
-      tags: req.body.tags,
-    };
+      const mockedDay = {
+        date: req.body.date,
+        score: req.body.score,
+        tags: req.body.tags,
+      };
 
-    const saveMock = jest.fn().mockResolvedValue(mockedDay);
-    jest.spyOn(Day.prototype, 'save').mockImplementation(saveMock);
+      const saveMock = jest.fn().mockResolvedValue(mockedDay);
+      jest.spyOn(Day.prototype, 'save').mockImplementation(saveMock);
 
-    await createDay(req, res);
+      await createDay(req, res);
 
-    expect(Day.prototype.save).toHaveBeenCalledTimes(1);
-    expect(res.status).toHaveBeenCalledWith(201);
-    expect(res.json).toHaveBeenCalledTimes(1);
-    expect(res.json).toHaveBeenCalledWith(req.body);
+      expect(Day.prototype.save).toHaveBeenCalledTimes(1);
+      expect(res.status).toHaveBeenCalledWith(201);
+      expect(res.json).toHaveBeenCalledTimes(1);
+      expect(res.json).toHaveBeenCalledWith(req.body);
 
-    Day.prototype.save.mockRestore();
+      Day.prototype.save.mockRestore();
+    });
+
+    it('should handle an empty request', async () => {
+      const req = { body: undefined };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      await createDay(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({ error: 'Missing request body' });
+    });
+
+    it('should handle a request with a missing parameter', async () => {
+      const req = {
+        body: {
+          date: '2023-06-04T20:49:58.178Z',
+          tags: ['bored', 'tired'],
+        },
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      await createDay(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({
+        error: 'Missing request field(s)',
+      });
+    });
   });
 
-  it('should handle an empty request', async () => {
-    const req = { body: undefined };
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
+  describe('get days', () => {
+    it('should get array of days and return 200', async () => {
+      const req = {};
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
 
-    await createDay(req, res);
+      const findMockData = [
+        {
+          date: '2023-06-04T20:49:58.178Z',
+          score: 1,
+          tags: ['bored', 'tired'],
+        },
+      ];
 
-    expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.json).toHaveBeenCalledWith({ error: 'Missing request body' });
-  });
+      const findMock = jest.fn().mockResolvedValue(findMockData);
+      jest.spyOn(Day, 'find').mockImplementation(findMock);
 
-  it('should handle a request with a missing parameter', async () => {
-    const req = {
-      body: {
-        date: '2023-06-04T20:49:58.178Z',
-        tags: ['bored', 'tired'],
-      },
-    };
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
+      await getDays(req, res);
 
-    await createDay(req, res);
+      expect(Day.find).toHaveBeenCalledTimes(1);
+      // there is no assertion for the response status to be 200, as this is implicitly set by Express for res.json()
+      expect(res.json).toHaveBeenCalledTimes(1);
+      expect(res.json).toHaveBeenCalledWith(findMockData);
 
-    expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.json).toHaveBeenCalledWith({
-      error: 'Missing request field(s)',
+      Day.find.mockRestore();
     });
   });
 });
